669911c02dd4861ecd5013988477b4a8cbea252f
Filip Kokosiński <filip.kokosinski@gmail.com>
2024-07-08 19:29:12 +0200

    gcc: hide accumulator from the compiler
    Signed-off-by: Filip Kokosiński <filip.kokosinski@gmail.com>

--- c-test-arith/main.c
+++ c-test-arith/main.c
@@ -1,8 +1,5 @@
-static void putc(int);
-
-int x;
-int y;
-int z;
+int x = 21;
+int y = 37;
 
 void _start(void)
 {
@@ -12,22 +9,57 @@ void _start(void)
 	asm volatile ("law 04000");
 	asm volatile ("dac 0130");
 
-	x = 21;
-	y = 37;
+	/* test addition with two variables */
+	*(volatile int *)(01000) = x + y;
 
-	z = x << 3;
-	if (z == 168)
-		putc(027);
-	
-	z = y >> 3;
-	if (z == 4)
-		putc(027);
+	/* test addition with an immediate value */
+	*(volatile int *)(01001) = x + 20;
+
+	/* test addition with an immediate value equal 1 */
+	*(volatile int *)(01002) = x + 1;
+
+	/* test subtraction with two variables */
+	*(volatile int *)(01003) = y - x;
+
+	/* test subtraction with an immediate value */
+	*(volatile int *)(01004) = y - 10;
+
+	/* test multiplication with two variables */
+	*(volatile int *)(01005) = x * y;
+
+	/* test division with two variables */
+	*(volatile int *)(01006) = y / x;
+
+	/* test modulo with two variables */
+	*(volatile int *)(01007) = y % x;
+
+	/* test logical negation with a variable */
+	*(volatile int *)(01010) = ~x;
+
+	/* test logical and with two variables */
+	*(volatile int *)(01011) = x & y;
+
+	/* test logical and with an immediate value */
+	*(volatile int *)(01012) = x & 12;
+
+	/* test logical or with two variables */
+	*(volatile int *)(01013) = x | y;
+
+	/* test logical or with an immediate value */
+	*(volatile int *)(01014) = x | 12;
+
+	/* test logical xor with two variables */
+	*(volatile int *)(01015) = x ^ y;
+
+	/* test logical xor with an immediate value */
+	*(volatile int *)(01016) = x ^ 12;
+
+	/* test shift left */
+	*(volatile int *)(01016) = x << 6;
+
+	/* test shift right */
+	*(volatile int *)(01017) = x >> 3;
 
 	asm volatile ("hlt");
 	__builtin_unreachable();
-}
-
-static void putc(int c) {
-	asm volatile ("lio %0" : : "r"(c) : "$io");
-	asm volatile ("tyo");
 }
